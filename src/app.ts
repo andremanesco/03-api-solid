14fbd1cc175bcc6556407929d60d28fa979ccfec
Andre Manesco <andrem_araujo@outlook.com>
2024-08-25 09:46:22 -0300

    create first route and controller

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
 import fastify from "fastify";
-import { PrismaClient } from '@prisma/client'
+import { register } from "./http/controllers/register";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-    data: {
-        name: 'andre',
-        email: 'andremanesco@hotmail.com'
-    }
-})
+app.register(appRoutes)
